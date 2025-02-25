3c3e7ef86346ba00738d71debd8669e1f38967d2
Hollis <bianyukun1213@outlook.com>
2025-02-25 20:09:52 +0800

    udpate

--- main.js
+++ main.js
@@ -153,7 +153,7 @@ function updateInterface(langKey) {
                         const moreContact = document.getElementById('more-contact');
                         if (!moreContact) return;
                         const res = await response.json();
-                        moreContact.innerHTML = marked.parse(res[currentLang]);
+                        moreContact.innerHTML = marked.parse(res[langKey]);
                     }).catch(function (error) {
                         throw console.error(`Unable to get more contact: Request failed with error ${error}.`);
                     });
