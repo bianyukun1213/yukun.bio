6146ff02abbb3a5500a807ddf0c5c384e884a4f6
Hollis <bianyukun1213@outlook.com>
2025-03-06 21:28:02 +0800

    update

--- res/main.js
+++ res/main.js
@@ -41,38 +41,20 @@ function variousFix() {
     }
 }
 
+let hideImgLoading;
+let showImgError;
 let initPswp;
 
 function updateInterface(langKey) {
     const langContent = content[langKey];
     document.documentElement.setAttribute('lang', langKey);
     document.documentElement.setAttribute('dir', langContent.dir);
-    const profile = document.getElementById('profile');
-    profile.setAttribute('src', langContent.profiles[0].src);
-    profile.setAttribute('alt', langContent.profiles[0].alt);
-    if (initPswp) {
-        profile.removeEventListener('click', initPswp);
-        profile.removeEventListener('keydown', initPswp);
-    }
-    initPswp = function (e) {
-        if (e.type === 'click' || e.type === 'keydown' && e.key === 'Enter') {
-            e.preventDefault();
-            const psOptions = {
-                dataSource: langContent.profiles
-            };
-            Object.assign(psOptions, langContent.photoSwipe);
-            const pswp = new PhotoSwipe(psOptions);
-            pswp.init();
-        }
-    };
-    profile.addEventListener('click', initPswp);
-    profile.addEventListener('keydown', initPswp);
     document.getElementById('name').textContent = document.title = langContent.name;
     document.getElementById('desc').textContent = langContent.desc;
     const links = document.getElementById('links');
     links.innerHTML = '';
     for (const link of langContent.links) {
-        const iconWrapper = document.createElement('span');
+        let iconWrapper = document.createElement('span');
         iconWrapper.setAttribute('class', 'icon-wrapper');
         const newLink = document.createElement('a');
         newLink.title = link.title;
@@ -88,17 +70,60 @@ function updateInterface(langKey) {
         el.textContent = langContent.colorScheme[el.value];
     });
     document.querySelector('label[for="select-lang"]').textContent = langContent.langLabel;
+    /** render markdown */
     document.getElementById('rendered-content').innerHTML = marked.parse(langContent.markdown);
+    const imgs = [...document.getElementsByTagName('img')];
+    imgs.forEach(function (img) {
+        img.className += ' img-loading';
+    });
+    hideImgLoading = function (e) {
+        let loadedImg = e.target;
+        loadedImg.className = loadedImg.className.replace(' img-loading', '');
+        loadedImg.style.aspectRatio = null;
+    };
+    showImgError = function (e) {
+        let loadedImg = e.target;
+        loadedImg.removeAttribute('tabindex');
+        loadedImg.className = loadedImg.className.replace(' img-loading', '') + ' img-error';
+    };
+    const profile = document.getElementById('profile');
+    if (hideImgLoading) profile.removeEventListener('load', hideImgLoading);
+    if (showImgError) profile.removeEventListener('error', showImgError);
+    profile.addEventListener('load', hideImgLoading);
+    profile.addEventListener('error', showImgError);
+    profile.style.aspectRatio = langContent.profiles[0].width / langContent.profiles[0].height;
+    profile.setAttribute('src', langContent.profiles[0].src);
+    profile.setAttribute('alt', langContent.profiles[0].alt);
+    if (initPswp) {
+        profile.removeEventListener('click', initPswp);
+        profile.removeEventListener('keydown', initPswp);
+    }
+    initPswp = function (e) {
+        if (e.target.className.includes('img-error')) return;
+        if (e.type === 'click' || e.type === 'keydown' && e.key === 'Enter') {
+            e.preventDefault();
+            const psOptions = {
+                dataSource: langContent.profiles
+            };
+            Object.assign(psOptions, langContent.photoSwipe);
+            const pswp = new PhotoSwipe(psOptions);
+            pswp.init();
+        }
+    };
+    profile.addEventListener('click', initPswp);
+    profile.addEventListener('keydown', initPswp);
     const renderedImgs = [...document.querySelectorAll('#rendered-content img[data-width][data-height]')];
     if (renderedImgs.length > 0) {
         renderedImgs.forEach(function (img) {
+            img.style.aspectRatio = img.getAttribute('data-width') / img.getAttribute('data-height');
             const initPswpRendered = function (e) {
+                if (e.target.className.includes('img-error')) return;
                 if (e.type === 'click' || e.type === 'keydown' && e.key === 'Enter') {
                     e.preventDefault();
                     const imgIndex = renderedImgs.indexOf(e.target);
                     if (imgIndex !== -1) {
                         const psRenderedOptions = {
-                            dataSource: renderedImgs.map(i => { return { src: i.src, width: i.getAttribute('data-width'), height: i.getAttribute('data-height'), alt: i.alt }; }),
+                            dataSource: renderedImgs.filter(i => !i.className.includes('img-error')).map(i => { return { src: i.src, width: i.getAttribute('data-width'), height: i.getAttribute('data-height'), alt: i.alt }; }),
                             index: imgIndex
                         };
                         Object.assign(psRenderedOptions, langContent.photoSwipe);
@@ -107,6 +132,8 @@ function updateInterface(langKey) {
                     }
                 }
             };
+            img.addEventListener('load', hideImgLoading);
+            img.addEventListener('error', showImgError);
             img.addEventListener('click', initPswpRendered);
             img.addEventListener('keydown', initPswpRendered);
         });
@@ -134,7 +161,6 @@ function domContentLoadedHandler(eDomContentLoaded) {
     }
     if (!currentLang)
         currentLang = 'en';
-
     let newUrl = url.origin + url.pathname;
     url.searchParams.delete('lang');
     let qs = url.searchParams.toString();
